bab6293dec10fb3e7159fc294c03c86ea6d368f5
xanthemc <39460939+xanthemc@users.noreply.github.com>
2023-02-14 14:22:30 +0800

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,6 +1,24 @@
 import * as React from 'react';
 import './style.css';
 
+let name: string = 'Piyush';
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+let printName: (name: string) => never; //void returns undefined, never doesnt return anything
+// let person: Object;
+//using type/interface to know the properties contains
+type Person = {
+  name: string;
+  age?: number; //optional
+};
+// role = [5, 'its a tuple'];
+let person: Person = {
+  name: 'piyush',
+};
+
+let lotsOfPeople: Person[];
 export default function App() {
   return (
     <div>
