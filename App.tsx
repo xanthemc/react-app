6c3a741c46e4f1cffd206fc11bae6964f30edf9c
xanthemc <39460939+xanthemc@users.noreply.github.com>
2023-02-14 14:39:57 +0800

    2 files modified

--- App.tsx
+++ App.tsx
@@ -8,11 +8,30 @@ let hobbies: string[];
 let role: [number, string];
 let printName: (name: string) => never; //void returns undefined, never doesnt return anything
 // let person: Object;
+
 //using type/interface to know the properties contains
-type Person = {
+interface Person {
   name: string;
   age?: number; //optional
+}
+/* interface extends easily */
+/* interface Guy extends Person {
+  profession: string;
+}
+type X = {
+  a: string;
+  b: number;
 };
+
+type Y = X & {
+  c: string;
+  d: number;
+};
+let y: Y = {
+  c: 'testing type',
+  d: 42,
+};
+ */
 // role = [5, 'its a tuple'];
 let person: Person = {
   name: 'piyush',
