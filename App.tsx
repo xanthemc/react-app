399f1792ad78ec05e98fe0a839d479e277ad1eda
xanthemc <39460939+xanthemc@users.noreply.github.com>
2023-02-15 21:47:20 +0800

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,48 +1,17 @@
 import * as React from 'react';
+import { Greet } from './components/Greet';
 import './style.css';
 
-let name: string = 'Piyush';
-let age: number;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-let printName: (name: string) => never; //void returns undefined, never doesnt return anything
-// let person: Object;
-
-//using type/interface to know the properties contains
 interface Person {
   name: string;
   age?: number; //optional
 }
-/* interface extends easily */
-/* interface Guy extends Person {
-  profession: string;
-}
-type X = {
-  a: string;
-  b: number;
-};
-
-type Y = X & {
-  c: string;
-  d: number;
-};
-let y: Y = {
-  c: 'testing type',
-  d: 42,
-};
- */
-// role = [5, 'its a tuple'];
-let person: Person = {
-  name: 'piyush',
-};
-
-let lotsOfPeople: Person[];
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <Greet name='Vishmas'>
     </div>
   );
 }
+
+export default App;
---
+++ components/Greet.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const Greet = () => {
+  return (
+    <div>
+      <h2>Welcome You have 10 unread messages</h2>
+    </div>
+  );
+};
