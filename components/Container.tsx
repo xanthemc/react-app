7f9a773057fdc870afef64fb1fadda218f32c6cf
xanthemc <39460939+xanthemc@users.noreply.github.com>
2023-02-16 16:23:37 +0800

    1 file added

---
+++ components/Container.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const Container = () => {
+  return (
+    <div style={{ border: '1px solid black', padding: '1rem' }}>
+      Text Content goes here
+    </div>
+  );
+};
