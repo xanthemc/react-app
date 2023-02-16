61ab37bb7d0fc8d89e4f125cf6787a473b80a914
xanthemc <39460939+xanthemc@users.noreply.github.com>
2023-02-16 17:29:27 +0800

    1 file added

---
+++ components/state/Counter.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { useReducer, useState } from 'react';
+
+const initialState = { count: 0 };
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + action.payload };
+    case 'decrement':
+      return { count: state.count - action.payload };
+    default:
+      return state;
+  }
+}
+
+export const Counter = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      Count:{state.count}
+      <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>
+        Increment 10
+      </button>
+      <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
+        Decrement 10
+      </button>
+    </div>
+  );
+};
